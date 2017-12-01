98322287181c2b110764e093da62ade78f04a9b2
Carson Brown <carsonbrown67@gmail.com>
2017-12-01 16:49:33 -0500

    More prints

--- tradingSimulator.py
+++ tradingSimulator.py
@@ -5,7 +5,7 @@ hist = pd.read_csv('ETH Hist.csv')
 prices = list()
 for i in hist['Close']:
     prices.insert(0, i)
-previous_count = .50
+previous_count = .5
 position = [0, 0]
 money = 10000
 
@@ -17,26 +17,30 @@ def simulate():
         decision = evaluate(prices[:previous], 7)
         position[0] += decision[0]
         position[1] += decision[1]
+        print "Current position " + str(position[0]) + " ETH with $" + str(position[1])
         money += decision[1]
         previous += 1
+    #print "$" + str(money) + " and " + str((position[0] * prices[-1])) + "ETH"
     return money + (position[0] * prices[-1])
 
 def evaluate(previous_data, period):
     #takes an array of previous prices
     #RETURNS: tuple of (coins exchanged, investment)
     #A buy will be a negative investment, a sell will be a positive investment
-    intervals = 10
-    matrix, ints = getMatrix(20, intervals, previous_data, period)
+    intervals = 5
+    matrix, ints = getMatrix(25, intervals, previous_data, period)
     prev = 100 * (1 - (previous_data[-1] / previous_data[-period]))
     probs = interval(prev, ints)
     up_prob = sum(matrix[probs][intervals/2+1:])
-    if up_prob > .6:
+    if up_prob > .6 and money > 0:
         return buy(money * up_prob, previous_data[-1])
-    if up_prob < .4 and position[0] > 0:
+    if up_prob < .15 and position[0] > 0:
         return sell(up_prob, previous_data[-1])
     return (0, 0)
 
 def buy(dollar_amount, price):
+    if dollar_amount > money:
+        dollar_amount = money
     coins = dollar_amount / price
     print "Bought " + str(coins) + " coins at $" + str(price)
     return coins, -dollar_amount
@@ -48,3 +52,5 @@ def sell(up_prob, price):
 
 if __name__ == '__main__':
     print simulate()
+    print "vs"
+    print str((10000/prices[int(previous_count * len(prices))]) * prices[-1]) + " return if held the entire time"
