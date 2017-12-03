cc86ccc3609f7563e7845d3e52634253c5b38ab5
benwallace15 <bwallace5417@gmail.com>
2017-12-02 21:02:13 -0500

    optimized version

--- tradingSimulator.py
+++ tradingSimulator.py
@@ -8,26 +8,27 @@ for i in hist['Close']:
 previous_count = .5
 position = [0, 0]
 
-def simulate(mon):
+def simulate(mon, bounds, intervals):
     money = mon
+    bnds = bounds
+    intrvls = intervals
     previous = previous_count * len(prices)
     previous = int(previous)
     for i in prices[previous:]:
-        decision = evaluate(prices[:previous], 7, money)
+        decision = evaluate(prices[:previous], 7, money, bnds, intrvls)
         position[0] += decision[0]
         position[1] += decision[1]
         #print "Current position " + str(position[0]) + " ETH with $" + str(position[1])
         money += decision[1]
         previous += 1
-    print "$" + str(money) + " and " + str((position[0] * prices[-1])) + "ETH"
+    #print "$" + str(money) + " and " + str((position[0] * prices[-1])) + "ETH"
     return money + (position[0] * prices[-1])
 
-def evaluate(previous_data, period, money):
+def evaluate(previous_data, period, money, bounds, intervals):
     #takes an array of previous prices
     #RETURNS: tuple of (coins exchanged, investment)
     #A buy will be a negative investment, a sell will be a positive investment
-    intervals = 10
-    matrix, ints = getMatrix(15, intervals, previous_data, period)
+    matrix, ints = getMatrix(bounds, intervals, previous_data, period)
     prev = 100 * (1 - (previous_data[-1] / previous_data[-period]))
     probs = interval(prev, ints)
     up_prob = sum(matrix[probs][intervals/2+1:])
@@ -41,16 +42,27 @@ def buy(dollar_amount, price, money):
     if dollar_amount > money:
         dollar_amount = money
     coins = dollar_amount / price
-    print "Bought " + str(coins) + " coins at $" + str(price)
+    #print "Bought " + str(coins) + " coins at $" + str(price)
     return coins, -dollar_amount
 
 def sell(up_prob, price):
     #sell = (1-up_prob) * position[0]
     sell = position[0]
-    print "Sold  " + str(sell) + " coins at $" + str(price)
+    #print "Sold  " + str(sell) + " coins at $" + str(price)
     return -sell, sell*price
 
 if __name__ == '__main__':
-    print simulate(10000)
-    print "vs"
-    print str((10000/prices[int(previous_count * len(prices))]) * prices[-1]) + " return if held the entire time"
+    maxretrn = 0
+    for i in range(10, 31):  #loop for bounds
+        for j in range(2, 15):  #loop for intervals
+            if (2 * i) % j == 0 and j<i:
+                retrn = simulate(10000, i, j)
+                if retrn > maxretrn:
+                    maxretrn = retrn
+                    maxBound = i
+                    maxInt = j
+    print("Money: " + str(maxretrn) + " | Bound: " + str(maxBound) + " | Intervals: " + str(maxInt))
+                #print("Money: " + str(retrn) + " | Bound: " + str(i) + " | Intervals: " + str(j))
+    #print simulate(10000, 15, 10)
+    print("vs")
+    print(str((10000/prices[int(previous_count * len(prices))]) * prices[-1]) + " return if held the entire time")
